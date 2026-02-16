e473c92e07021a25c617dade374e4cbfa9d39538
Thunder <aashubaghel08@gmail.com>
2026-02-16 19:52:12 +0530

    sdsd

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ RandImageArray[3] = 'assets/homepage/landscape4.png';
 var num = Math.floor(Math.random()*4);
 document.getElementById("bannerImage").src = RandImageArray[num];
 console.log("Loaded banner Image:",RandImageArray[num]);
+
+const githubURL="https://api.github.com/repositories/1135079675"
+fetch(githubURL)
+	.then(response => {
+		if(!response.ok){
+			throw new Error(`HTTP Error! Status: ${response.status}`);
+		}
+		return response.json();
+	})
+	.then(data => {
+		console.log(data);
+		const updatedDate=new Date(data.updated_at).toLocaleDateString();
+		document.getElementById("lastPageUpdatedDate").innerHTML=updatedDate
+	})
+	.catch(error => {	
+		console.error('Error:', error);
+	});
